f7beb4107d02e675415de8127e5b143b5c34a9cd
Keanu <keanusantosgermanopereira@gmail.com>
2026-02-22 18:51:30 -0300

    feat: User create and put and summary DTOs

---
+++ src/main/java/com/keanusantos/personalfinancemanager/domain/user/dto/request/CreateUserDTO.java
@@ -0,0 +1,12 @@
+package com.keanusantos.personalfinancemanager.domain.user.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public record CreateUserDTO(
+        @NotEmpty(message = "The name must not be empty") @Size(min = 3, max = 20, message = "Name must be between 3 and 20 characters long") String name,
+        @NotEmpty(message = "The email must not be empty") @Email(message = "Invalid email") String email,
+        @NotEmpty(message = "The password must not be empty") @Size(min = 4, max = 6, message = "password must be between 4 and 6 characters long") String password
+) {
+}
---
+++ src/main/java/com/keanusantos/personalfinancemanager/domain/user/dto/summary/UserSummaryDTO.java
@@ -0,0 +1,7 @@
+package com.keanusantos.personalfinancemanager.domain.user.dto.summary;
+
+public record UserSummaryDTO(
+        Long id,
+        String name
+) {
+}
