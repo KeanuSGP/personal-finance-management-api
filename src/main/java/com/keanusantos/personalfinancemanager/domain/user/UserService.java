7aeabd8840d568816dd65ddd53d05d1065e64bd5
Keanu <keanusantosgermanopereira@gmail.com>
2026-02-22 05:52:30 -0300

    refactor: change method returns to userResponseDTO

--- src/main/java/com/keanusantos/personalfinancemanager/domain/user/UserService.java
+++ src/main/java/com/keanusantos/personalfinancemanager/domain/user/UserService.java
@@ -1,5 +1,7 @@
 package com.keanusantos.personalfinancemanager.domain.user;
 
+import com.keanusantos.personalfinancemanager.domain.user.dto.mapper.UserDTOMapper;
+import com.keanusantos.personalfinancemanager.domain.user.dto.response.UserResponseDTO;
 import com.keanusantos.personalfinancemanager.exception.BusinessException;
 import com.keanusantos.personalfinancemanager.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -14,23 +17,25 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
-    public List<User> findAll() {
-        return repository.findAll();
+    public List<UserResponseDTO> findAll() {
+        return repository.findAll().stream().map(UserDTOMapper::toResponse).collect(Collectors.toList());
     }
 
-    public User findById(Long id) {
-        return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
+    public UserResponseDTO findById(Long id) {
+       return UserDTOMapper.toResponse(repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id)));
     }
 
-    public User insert(User obj) {
+    public UserResponseDTO insert(User obj) {
         validateUser(obj);
-        return repository.save(obj);
+        repository.save(obj);
+        return UserDTOMapper.toResponse(obj);
     }
 
-    public User update(Long id, User obj) {
+    public UserResponseDTO update(Long id, User obj) {
             User entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
             updateUserData(entity, obj);
-            return repository.save(entity);
+            repository.save(entity);
+            return  UserDTOMapper.toResponse(entity);
     }
 
     private void updateUserData(User entity, User obj) {
