cc3cee29771ac9c70d1556116d381b726e157897
Keanu <keanusantosgermanopereira@gmail.com>
2026-02-25 17:50:50 -0300

    refactor: remove unused imports

--- src/main/java/com/keanusantos/personalfinancemanager/domain/transaction/dto/response/TransactionResponseDTO.java
+++ src/main/java/com/keanusantos/personalfinancemanager/domain/transaction/dto/response/TransactionResponseDTO.java
@@ -1,16 +1,11 @@
 package com.keanusantos.personalfinancemanager.domain.transaction.dto.response;
 
-import com.keanusantos.personalfinancemanager.domain.category.Category;
 import com.keanusantos.personalfinancemanager.domain.category.dto.summary.CategorySummaryDTO;
-import com.keanusantos.personalfinancemanager.domain.transaction.Installment;
+import com.keanusantos.personalfinancemanager.domain.transaction.installment.Installment;
 import com.keanusantos.personalfinancemanager.domain.transaction.dto.summary.CounterPartySummaryDTO;
 import com.keanusantos.personalfinancemanager.domain.transaction.dto.summary.FinancialAccountSummaryDTO;
 import com.keanusantos.personalfinancemanager.domain.transaction.enums.TransactionType;
-import com.keanusantos.personalfinancemanager.domain.user.User;
-import com.keanusantos.personalfinancemanager.domain.user.dto.response.UserResponseDTO;
 import com.keanusantos.personalfinancemanager.domain.user.dto.summary.UserSummaryDTO;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 
 import java.util.List;
 import java.util.Set;
