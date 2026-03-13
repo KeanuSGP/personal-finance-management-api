e251e92be0ba4401ba64a029553a763c325d1268
Keanu <keanusantosgermanopereira@gmail.com>
2026-03-13 18:46:03 -0300

    refactor: remove user from DTOs

--- src/main/java/com/keanusantos/personalfinancemanager/domain/category/dto/request/PutCategoryDTO.java
+++ src/main/java/com/keanusantos/personalfinancemanager/domain/category/dto/request/PutCategoryDTO.java
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.Size;
 
 public record PutCategoryDTO(
         @NotEmpty(message = "The name must not be empty") @Size(min = 3, max = 20, message = "Name must be between 3 and 20 characters long") String name,
-        @NotEmpty(message = "The color must not be empty") @Size(min = 6, max = 6, message = "Color must have 6 characters long") String color,
-        @NotNull(message = "The user must not be null") Long user
+        @NotEmpty(message = "The color must not be empty") @Size(min = 6, max = 6, message = "Color must have 6 characters long") String color
 ) {
 }
