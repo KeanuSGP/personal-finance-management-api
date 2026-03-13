78b89f08a46f02d0ae123aef648bf8dd580a6f02
Keanu <keanusantosgermanopereira@gmail.com>
2026-03-13 18:53:26 -0300

    refactor: remove entity id

--- src/main/java/com/keanusantos/personalfinancemanager/exception/ResourceNotFoundException.java
+++ src/main/java/com/keanusantos/personalfinancemanager/exception/ResourceNotFoundException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 
 public class ResourceNotFoundException extends BusinessException {
 
-    public ResourceNotFoundException(Long id) {
-        super("Resource not found. id " + id, HttpStatus.NOT_FOUND);
+    public ResourceNotFoundException() {
+        super("Resource not found", HttpStatus.NOT_FOUND);
     }
 
 }
