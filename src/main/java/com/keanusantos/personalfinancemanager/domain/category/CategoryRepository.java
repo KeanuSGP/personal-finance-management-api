55fbc9b66fd8b90f5f00748190713ea61826ba6f
Keanu <keanusantosgermanopereira@gmail.com>
2026-03-13 18:52:09 -0300

    feat: implement resource ownership validation

--- src/main/java/com/keanusantos/personalfinancemanager/domain/category/CategoryRepository.java
+++ src/main/java/com/keanusantos/personalfinancemanager/domain/category/CategoryRepository.java
@@ -1,12 +1,27 @@
 package com.keanusantos.personalfinancemanager.domain.category;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    boolean existsByNameAndIdNot(String name, Long id);
-    boolean existsByColorAndIdNot(String color, Long id);
-    boolean existsByName(String name);
-    boolean existsByColor(String color);
+    boolean existsByNameAndUserIdAndIdNot(String name, Long userId, Long id);
+    boolean existsByColorAndUserIdAndIdNot(String color, Long userId, Long id);
+    boolean existsByNameAndUserId(String name,  Long id);
+    boolean existsByColorAndUserId(String color, Long id);
+    Set<Category> findAllByIdInAndUser_id(Set<Long> ids, Long user);
+    Set<Category> findAllByUser_id(Long id);
+    Optional<Category> findByIdAndUser_Id(Long id, Long user);
+    boolean existsByIdAndUserId(Long id, Long userId);
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM transaction_category WHERE category_id = :id", nativeQuery = true)
+    void deleteReferencesById(@Param("id") Long id);
 }
