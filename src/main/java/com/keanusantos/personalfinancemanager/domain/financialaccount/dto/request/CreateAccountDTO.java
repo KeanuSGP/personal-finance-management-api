a74a73f52ee2bc1a9196c2a2782be06f21e688ef
Keanu <keanusantosgermanopereira@gmail.com>
2026-04-03 15:14:28 -0300

    refactor: remove user property from dto

--- src/main/java/com/keanusantos/personalfinancemanager/domain/financialaccount/dto/request/CreateAccountDTO.java
+++ src/main/java/com/keanusantos/personalfinancemanager/domain/financialaccount/dto/request/CreateAccountDTO.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.*;
 
 public record CreateAccountDTO (
         @NotEmpty(message = "The name must not be empty") @Size(min = 3, max = 20, message = "Name must be between 3 and 20 characters long") String name,
-        @NotNull(message = "The balance must not be null") @Min(value = 0, message = "The balance must be zero ou greater") Float balance,
-        @NotNull(message = "The user must not be null") Long user
+        @NotNull(message = "The balance must not be null") @Min(value = 0, message = "The balance must be zero ou greater") Float balance
 ){
 }
