5a223d39ebfd4e518968fad613764bd4ba375f40
Keanu <keanusantosgermanopereira@gmail.com>
2026-05-09 18:22:27 -0300

    feat: add login method

--- src/main/java/com/keanusantos/personalfinancemanager/domain/auth/AuthController.java
+++ src/main/java/com/keanusantos/personalfinancemanager/domain/auth/AuthController.java
@@ -1,49 +1,38 @@
 package com.keanusantos.personalfinancemanager.domain.auth;
 
 import com.keanusantos.personalfinancemanager.config.security.TokenService;
-import com.keanusantos.personalfinancemanager.config.security.UserDetailsImpl;
-import com.keanusantos.personalfinancemanager.config.security.UserDetailsServiceImpl;
 import com.keanusantos.personalfinancemanager.domain.auth.request.AuthDTO;
+import com.keanusantos.personalfinancemanager.domain.auth.response.AuthResponseDTO;
 import com.keanusantos.personalfinancemanager.domain.user.UserService;
 import com.keanusantos.personalfinancemanager.domain.user.dto.request.CreateUserDTO;
 import com.keanusantos.personalfinancemanager.domain.user.dto.response.UserResponseDTO;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/auth")
 public class AuthController {
 
     @Autowired
-    private UserDetailsServiceImpl userDetailsServiceImpl;
-    @Autowired
-    private AuthenticationManager authManager;
-    @Autowired
     private TokenService tokenService;
     @Autowired
     private UserService userService;
+    @Autowired
+    private AuthService authService;
 
     @PostMapping(value = "/login")
-    public String Login(@RequestBody @Valid AuthDTO dto) {
-        UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(dto.name(), dto.password());
-        Authentication authentication = authManager.authenticate(userAndPass);
-        String token = tokenService.generateToken((UserDetailsImpl) authentication.getPrincipal());
-        return token;
+    public AuthResponseDTO Login(@RequestBody @Valid AuthDTO dto) {
+        AuthResponseDTO response = authService.login(dto);
+        return response;
     }
-
 
     @PostMapping(value = "/register")
     public UserResponseDTO Register(@RequestBody @Valid CreateUserDTO dto) {
         return userService.insert(dto);
-    }
-
-    @GetMapping
-    public String test() {
-        return "Successful";
     }
 }
--- src/main/java/com/keanusantos/personalfinancemanager/domain/auth/AuthService.java
+++ src/main/java/com/keanusantos/personalfinancemanager/domain/auth/AuthService.java
@@ -1,9 +1,20 @@
 package com.keanusantos.personalfinancemanager.domain.auth;
 
+import com.keanusantos.personalfinancemanager.config.security.TokenService;
 import com.keanusantos.personalfinancemanager.config.security.UserDetailsImpl;
+import com.keanusantos.personalfinancemanager.config.security.UserDetailsServiceImpl;
+import com.keanusantos.personalfinancemanager.domain.auth.mapper.AuthDTOMapper;
+import com.keanusantos.personalfinancemanager.domain.auth.request.AuthDTO;
+import com.keanusantos.personalfinancemanager.domain.auth.response.AuthResponseDTO;
 import com.keanusantos.personalfinancemanager.domain.user.User;
+import com.keanusantos.personalfinancemanager.domain.user.dto.mapper.UserDTOMapper;
+import com.keanusantos.personalfinancemanager.domain.user.dto.response.UserResponseDTO;
 import com.keanusantos.personalfinancemanager.exception.BusinessException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,6 +22,26 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
+    @Autowired
+    AuthenticationManager authManager;
+    @Autowired
+    TokenService tokenService;
+    @Autowired
+    UserDetailsServiceImpl userDetailsImpl;
+
+    public AuthResponseDTO login(AuthDTO credentials) {
+        try {
+            UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(credentials.name(), credentials.password());
+            Authentication auth = authManager.authenticate(userAndPass);
+            String token = tokenService.generateToken((UserDetailsImpl) auth.getPrincipal());
+            User user = ((UserDetailsImpl) userDetailsImpl.loadUserByUsername(credentials.name())).getUser();
+            UserResponseDTO toResponse = UserDTOMapper.toResponse(user);
+            AuthResponseDTO authResponseDTO = AuthDTOMapper.toAuthResponseDTO(toResponse, token);
+            return authResponseDTO;
+        } catch (BadCredentialsException e) {
+            throw new BusinessException(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 
     public User getAuthenticatedUser() {
 
